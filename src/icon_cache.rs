35b90006a73c81df501c80f2527ac28c94b396a9
ShootingStarDragons <ShootingStarDragons@protonmail.com>
2023-10-10 22:02:12 +0800

    chore: use rwlock instead

--- src/icon_cache.rs
+++ src/icon_cache.rs
@@ -4,7 +4,7 @@ use once_cell::sync::Lazy;
 
 use std::{
     collections::HashMap,
-    sync::{Arc, Mutex},
+    sync::{Arc, RwLock},
 };
 
 #[derive(Debug, Hash, PartialEq, Eq)]
@@ -17,14 +17,16 @@ const TEXT_IMAGE: &[u8] = include_bytes!("../resources/text-plain.svg");
 
 const DIR_IMAGE: &[u8] = include_bytes!("../resources/inode-directory.svg");
 
-static ICON_CACHE: Lazy<Arc<Mutex<HashMap<IconKey, svg::Handle>>>> =
-    Lazy::new(|| Arc::new(Mutex::new(HashMap::new())));
+static ICON_CACHE: Lazy<Arc<RwLock<HashMap<IconKey, svg::Handle>>>> =
+    Lazy::new(|| Arc::new(RwLock::new(HashMap::new())));
 
 pub fn get_icon_handle(key: IconKey) -> svg::Handle {
-    let mut icon_cache = ICON_CACHE.lock().unwrap();
+    let icon_cache = ICON_CACHE.read().unwrap();
     if icon_cache.contains_key(&key) {
         return icon_cache.get(&key).unwrap().clone();
     }
+    drop(icon_cache);
+    let mut icon_cache = ICON_CACHE.write().unwrap();
     if let IconKey::Path(ref path) = key {
         let handle = svg::Handle::from_path(path);
         icon_cache.insert(key, handle.clone());
